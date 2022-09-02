a0a3752d83802001d72ae2b8748491d1b248e84a
L <sebasgarzons88@gmail.com>
2022-09-02 13:04:34 -0400

    Second task v.I

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -25,6 +25,7 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     return message
 
 def get_logger() -> logging.Logger[Object]:
+    """ Return a logging.Logger object"""
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
