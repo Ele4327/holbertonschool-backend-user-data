cd5e144b6bf45bc1885d0dd71c8ea75816ee2139
L <sebasgarzons88@gmail.com>
2022-09-02 19:25:45 -0400

    Test

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,8 @@
 from typing import List
 import re
 import logging
+import mysql.connector
+from os import environ
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
@@ -38,6 +40,19 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to the database object"""
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    return (mysql.connector.connection.MySQLConnection(user=username,
+                                                       password=password,
+                                                       host=host,
+                                                       database=db_name))
 
 
 class RedactingFormatter(logging.Formatter):
