8ee8f63f9ef5cc5e2af80b89535e2891efb3d11b
L <sebasgarzons88@gmail.com>
2022-09-02 21:12:23 -0400

    Encrypt Password. Valid. 6th Task - Fixed PyCodeStyle

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -16,7 +16,6 @@ def hash_password(password: str) -> bytes:
     return (bcrypt.hashpw(password.encode(), bcrypt.gensalt()))
 
 
-
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
         Implement an is_valid function that expects
