52dc6761ca6050ee056258fa1d1eab062f50083c
L <sebasgarzons88@gmail.com>
2022-09-02 21:10:02 -0400

    Encrypt Password. Valid. 6th Task

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,17 @@ def hash_password(password: str) -> bytes:
         hashed password, which is a byte string.
     """
     return (bcrypt.hashpw(password.encode(), bcrypt.gensalt()))
+
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        Implement an is_valid function that expects
+        2 arguments and returns a boolean.
+
+        Arguments:
+
+        hashed_password: bytes type
+        password: string type
+    """
+    return (bcrypt.checkpw(bytes(password.encode(), hashed_password)))
