57b71e0eec24e67a710a490c639a35b652f0fb90
L <sebasgarzons88@gmail.com>
2022-09-06 20:27:47 -0400

    Dont get it what is missing in requirements

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,13 +11,13 @@ class Auth:
     """ Authentication class to manage API requests"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """  """
+        """ Test """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """ """
+        """ Test """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ """
+        """ Test """
         return None
