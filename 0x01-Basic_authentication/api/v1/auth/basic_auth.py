a0f2325ab6db0382ee8eac5308b70ad7d4082beb
L <sebasgarzons88@gmail.com>
2022-09-06 21:46:17 -0400

    Task 6 to 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,65 @@
 
 
 class BasicAuth(Auth):
-    """
-        Test
-    """
+    """Test"""
+
+    def extract_base64_authorization_header(self, authorization_header: str)\
+            -> str:
+        """ Test """
+
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if "Basic " not in authorization_header:
+            return None
+
+        return authorization_header.split("Basic ", 1)[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Test """
+
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Test """
+
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+
+        return decoded_base64_authorization_header.split(":", 1)[0], \
+            decoded_base64_authorization_header.split(":", 1)[1]
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ Test """
+
+        if user_email is None or user_pwd is None:
+            return None
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            search_users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        for user in search_users:
+            if user.is_valid_password(user_pwd):
+                return user
+            else:
+                return None
