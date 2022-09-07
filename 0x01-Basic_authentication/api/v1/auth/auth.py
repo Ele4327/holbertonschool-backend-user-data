b7558d59990752d4d30cbb5e2efd13c7a0961a77
L <sebasgarzons88@gmail.com>
2022-09-06 20:16:55 -0400

    Auth def user added and typing ex-erased

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,12 +8,16 @@ from typing import List, TypeVar
 
 
 class Auth:
-    """ Authentication """
+    """ Authentication class to manage API requests"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """  """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
         """ """
         return None
