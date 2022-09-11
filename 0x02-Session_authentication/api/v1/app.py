1b68f6b2f33c6de2a08375e9013527325a3ddfa8
L <sebasgarzons88@gmail.com>
2022-09-10 21:13:44 -0400

    App.py is all documented

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+    Route module for the API
 """
+
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
@@ -27,20 +28,19 @@ elif auth == "session_auth":
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """Unauthorized handler"""
+    """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """Forbidden handler"""
+    """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
 
 
