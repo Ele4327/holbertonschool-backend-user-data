b125c1900e460bbeef030841015306658fe4a60b
L <sebasgarzons88@gmail.com>
2022-09-16 00:16:02 -0400

    Tests

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,10 +34,10 @@ class DB:
         """
             Save user to database
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        return new_user
+        return user
 
     def find_user_by(self, **kwargs) -> User:
         """
